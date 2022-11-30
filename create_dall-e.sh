da13b802317de0f95338e16012986fddfb53a966
James Lownie <jlownie@hotmail.com>
2022-11-30 19:39:12 +1100

    First version of the script

---
+++ create_dall-e.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROMPT=$(cat prompt.txt)
+OUTFILE=image.png
+KEY=$(cat openai_key)
+
+curl https://api.openai.com/v1/images/generations \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $KEY" \
+-d "{
+    \"response_format\": \"b64_json\",
+    \"prompt\": \"$PROMPT\",
+    \"n\": 1,
+    \"size\": \"256x256\"
+}" | jq .data[].b64_json | tr -d '"' | base64 -d > $OUTFILE
